8f6e3cfe5ba16209b299db9631bb486ae29cffc8
Rasmus Eskola <fruitiex@gmail.com>
2015-04-04 17:55:22 +0300

    include a throwaway API key

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Setup
 * Enable backend `youtube` in: `~/.nodeplayer/config/core.json`
 * Run nodeplayer once to generate sample config file: `npm start`
 * Edit `~/.nodeplayer/config/youtube.json`. You must provide a valid API key and
-  your country code.
+  your country code. A default is provided but it's not guaranteed to work.
 * API key can be generated in the Google Developer Console by creating a new
   project and enabling YouTube Data API v3 for it. The API key is then found in
   APIs & auth -> Credentials -> Create new Key -> Server key -> API KEY
+* Country code can be selected from (this)[http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements] list.
--- default-config.js
+++ default-config.js
@@ -1,7 +1,15 @@
 var defaultConfig = {};
 
-defaultConfig.apiKey = 'get your server api key from the google developer console';
+// It's advicable to generate your own key and use it instead.
+// The default may stop working at any time, see README.md
+defaultConfig.apiKey = 'AIzaSyB3CsRUP-IEz87_pqIct1c1HInmec9s7fY';
+
+// Change this to your own region code to get correct search results
+// for your region. See README.md
 defaultConfig.regionCode = 'FI';
-defaultConfig.maxScore = 50; // don't prefer youtube over other backends
+
+// Youtube provides very accurate search results, but sometimes they
+// are not of high quality. Other backends may take precedence.
+defaultConfig.maxScore = 50;
 
 module.exports = defaultConfig;
